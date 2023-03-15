c4f1d0c52f4b74d027b96aa54f76142e1725857a
nwankwolinus <51293362+nwankwolinus@users.noreply.github.com>
2023-03-15 05:40:11 +0100

    add my files

---
+++ main.js
@@ -0,0 +1,61 @@
+let shop = document.getElementById("shop");
+
+let shopItemsData = [{
+    id: "hdhjffhv",
+    name: "Casual Shirt",
+    price: 45,
+    desc: "lorem ipsum dolor sit amet consecttetur laprt",
+    img: "images/img-1.jpg"
+},
+{
+    id: "apsoedlj",
+    name: "Office Shirt",
+    price: 100,
+    desc: "lorem ipsum dolor sit amet consecttetur laprt",
+    img: "images/img-2.jpg"
+}, 
+{
+    id: "sjdhchdj",
+    name: "T Shirt",
+    price: 25,
+    desc: "lorem ipsum dolor sit amet consecttetur laprt",
+    img: "images/img-3.jpg"
+}, 
+{
+    id: "rirftjuy",
+    name: "Men's Suit",
+    price: 300,
+    desc: "lorem ipsum dolor sit amet consectetur laprt",
+    img: "images/img-4.jpg"
+},
+];
+
+let generateShop = () => {
+    return (shop.innerHTML = shopItemsData.map((x)=>{
+        let {id, name, price, desc, img } = x;
+        return `
+        <div id=product-id-${id} class="item">
+                <img width="220" src=${img} alt="">
+                <div class="details">
+                    <h3>${name}</h3>
+                    <p>${desc}</p>
+                    <div class="price-quantity">
+                        <h2>$ ${price}</h2>
+                        <div class="buttons">
+                            <i class="bi bi-dash-lg"></i>
+                            <div id=${id} class="quantity">0</div>
+                            <i class="bi bi-plus-lg"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `
+    }).join(""));
+};
+
+generateShop();
+
+let increment = () => {};
+let decrement = () => {};
+let update = () => {};
+
