7324026cd57f31dc6f895ca29a21eac8cc892d82
nwankwolinus <51293362+nwankwolinus@users.noreply.github.com>
2023-03-16 04:38:44 +0100

    modified my files

--- main.js
+++ main.js
@@ -30,6 +30,8 @@ let shopItemsData = [{
 },
 ];
 
+// let basket = [];
+let basket = JSON.parse(localStorage.getItem("data")) || []
 let generateShop = () => {
     return (shop.innerHTML = shopItemsData.map((x)=>{
         let {id, name, price, desc, img } = x;
@@ -42,9 +44,9 @@ let generateShop = () => {
                     <div class="price-quantity">
                         <h2>$ ${price}</h2>
                         <div class="buttons">
-                            <i class="bi bi-dash-lg"></i>
+                            <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
                             <div id=${id} class="quantity">0</div>
-                            <i class="bi bi-plus-lg"></i>
+                            <i onclick="increment(${id})" class="bi bi-plus-lg"></i>
                         </div>
                     </div>
                 </div>
@@ -55,7 +57,44 @@ let generateShop = () => {
 
 generateShop();
 
-let increment = () => {};
-let decrement = () => {};
-let update = () => {};
+let increment = (id) => {
+    let selectedItem = id;
+    let search = basket.find((x) => x.id === selectedItem.id);
+
+    if (search === undefined) {
+        basket.push({
+            id: selectedItem.id,
+            item: 1,
+        });
+    }
+    else {
+        search.item += 1;
+    }
+    localStorage.setItem("data", JSON.stringify(basket));
+    // console.log(basket);
+    update (selectedItem.id);
+};
+let decrement = (id) => {
+    let selectedItem = id;
+    let search = basket.find((x) => x.id === selectedItem.id);
+
+    if (search.item === 0) return;
+    else {
+        search.item -= 1;
+    }
+    localStorage.setItem("data", JSON.stringify(basket));
+    // console.log(basket);
+    update (selectedItem.id);
+};
+let update = (id) => {
+    let search = basket.find((x) => x.id === id);
+    // console.log(search.item);
+    document.getElementById(id).innerHTML = search.item;
+    calculation();
+};
+
+let calculation = () => {
+    let cartIcon = document.getElementById("cartAmount");
+    cartIcon.innerHTML = basket.map((x) => x.item).reduce((x,y) => x + y, 0);
+};
 
